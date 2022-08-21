615339182bbf9652085539a734e6dd8022dfdaa1
LieNautz <tuan_lai01@hotmail.com>
2022-08-21 12:15:35 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let cats =['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+let newArr=[...cats,name];
+return newArr
+}
+
+function prependCat(name){
+    let newArr=[name,...cats];
+    return newArr
+}
+
+function removeLastCat(){
+    let newArr=[...cats.slice(0,2)];
+    return newArr;
+    
+}
+
+function removeFirstCat(){
+    let newArr=[...cats.slice(1)];
+    return newArr
+}
